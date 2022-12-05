5a3a0a93ec3766900d4c6c86c7bb0449e55a995e
broline <brolineoronu3@gmail.com>
2022-12-05 12:50:27 -0800

    last fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
+    return new_string

3af939b71ecadcba480460e53356f471ef226f49
broline <brolineoronu3@gmail.com>
2022-12-19 02:59:23 -0800

    update

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-def safe_print_integer_err(value):
-    import sys
+
+import sys
+
+
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
-        return True
-    except Exception as i:
-        print("Exception: {}".format(i), file=sys.stderr)
-        return False
+        result = fct(*args)
+        return (result)
+    except:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)

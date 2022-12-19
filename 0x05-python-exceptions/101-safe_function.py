1536272b87ae7f67644f0f5d3bade7b97aebd14e
broline <brolineoronu3@gmail.com>
2022-12-19 02:57:06 -0800

    a function that executes a function safely.

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as i:
+        print("Exception: {}".format(i), file=sys.stderr)
+        return False
